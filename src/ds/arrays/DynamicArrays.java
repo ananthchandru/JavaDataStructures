1b27238123a2d00005176b54c600eabeccc3ca32
Anantha Subramanian K <ananths1006@gmail.com>
2021-06-08 20:28:15 +0530

    Commit additional folders

--- src/ds/arrays/DynamicArrays.java
+++ src/ds/arrays/DynamicArrays.java
@@ -1,5 +1,45 @@
 package ds.arrays;
 
+
+//First we need members and constructors to initialise the array.
 public class DynamicArrays {
+	
+	private int[] array;
+	private int size;
+	private int count;
+	
+	public DynamicArrays() {
+		array = new int[1];
+		size = 1;
+		count = 0;
+	}
+	
+	public void add(int element) {
+		if(size == count) {
+			growArray();
+		}
+		array[count] = element;
+		count++;
+	}
+	
+	public void growArray() {
+		int temp[] = new int[2*size];
+		for(int i=0;i<size; i++ ) {
+			temp[i] = array[i];
+		}
+		array = temp;
+		size = 2*size;
+	}
+	
+	public static void main(String[] args) {
+		DynamicArrays arr = new DynamicArrays();
+		
+		arr.add(5);
+		System.out.println(arr.size);
+		arr.add(6);
+		System.out.println(arr.size);
+		arr.add(7);
+		System.out.println(arr.size);
+	}
 
 }
