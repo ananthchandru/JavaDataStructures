3bf56f8fa4d8c11b2627e19dd73419242cb4a3d7
Anantha Subramanian <ananths1006@google.com>
2021-08-16 19:00:01 +0530

    Dynamic Arrays Improvisation

--- src/ds/arrays/DynamicArraySorted.java
+++ src/ds/arrays/DynamicArraySorted.java
@@ -16,69 +16,69 @@ public class DynamicArraySorted {
         if(size==count)
             growArray();
 
-        int index = findIndex(true, element, 0 ,count-1);
+        int index = -1;
+        //condition 1 => element must be lower than all other elements
+        if(count == 0 || element <= arr[0])
+            index =0;
+       //condition 2 => element is highest than all other elements
+        else if (element >= arr[count-1])
+            index = count;
+        //condition 3 => element must be within low and high
+        else
+            index = binarySearch(true, element, 0 ,count-1);
+
         for(int i=count-1; i>=index; i--)
             arr[i+1] = arr[i];
         arr[index] = element;
         count++;
+
+        printArray();
     }
 
     public void remove(int element) {
-        int index = findIndex(false, element, 0, count-1);
-        removeAt(index);
+        int index = binarySearch(false, element, 0, count-1);
+        if(index == -1)
+            System.out.println("Unable to find element");
+        else
+            removeAt(index);
     }
 
     public void removeAt(int index) {
-        if(count > 0) {
-            for(int i=index; i<count-1;i++)
-                arr[i] = arr[i+1];
-            count--;
-            arr[count] = 0;
+        for(int i=index; i<count-1;i++)
+            arr[i] = arr[i+1];
+        count--;
+        arr[count] = 0;
 
-            if(count < size/2) {
-                shrinkArray();
-            }
-        } else {
-            System.out.println("Unable to remove. List is empty");
-        }
+        if(count < size/2)
+            shrinkArray();
+        printArray();
     }
 
-    public int findIndex(boolean isInsert, int element, int low, int high) {
-
-        int mid = (high + low) / 2;
-
-        //find index for insert
-        if(isInsert) {
-            //condition 1 => element might be 0 or be first insert
-            if(element == 0 || count == 0 || element < arr[low])
-                return 0;
-            //condition 2 => element might be higher than high
-            if(element > arr[high])
-                return high+1;
-            //condition 3 => between adjacent indices
-            if (element > arr[mid] && element < arr[mid + 1])
+    public int binarySearch(boolean isInsert, int element, int low, int high) {
+            int mid = (high + low) / 2;
+            //condition 4 => element is equal to mid
+            if (element == arr[mid])
+                return mid;
+            //condition 5 => between adjacent indices
+            if (isInsert && element > arr[mid] && element < arr[mid + 1])
                 return mid + 1;
-        }
-        //condition 4 => element is equal to mid
-        if(element == arr[mid])
-            return mid;
-        //condition 5 => element is less than mid
-        if (element < arr[mid])
-            return findIndex(isInsert, element, low, mid-1);
-        //condition 6 => element is greater than mid
-        return findIndex(isInsert, element, mid+1, high);
+            //condition 6 => element is less than mid
+            if (element < arr[mid])
+                return binarySearch(isInsert, element, low, mid - 1);
+            //condition 7 => element is greater than mid
+            return binarySearch(isInsert, element, mid + 1, high);
     }
 
     public void growArray() {
-        size = 2 * size;
-        int[] temp = Arrays.copyOf(arr, size);
+        int[] temp = Arrays.copyOf(arr, size*2);
         arr = temp;
+        size = 2 * size;
     }
 
     public void shrinkArray() {
-        size = size/2;
-        int[] temp = Arrays.copyOf(arr, size);
+        int[] temp = Arrays.copyOf(arr, size/2);
         arr = temp;
+        size = size/2;
     }
 
     public static void main(String[] args) {
@@ -90,5 +90,9 @@ public class DynamicArraySorted {
         array.remove(3);
         array.removeAt(1);
         array.remove(7);
+    }
+
+    public void printArray() {
+        System.out.println(Arrays.toString(arr));
     }
 }
--- src/ds/arrays/DynamicArrays.java
+++ src/ds/arrays/DynamicArrays.java
@@ -1,19 +1,19 @@
 package ds.arrays;
 
-
 import java.util.Arrays;
 
 //First we need members and constructors to initialise the array.
 //reference - https://www.geeksforgeeks.org/how-do-dynamic-arrays-work/
+
 public class DynamicArrays {
 	
 	private int[] array;
 	private int size;
 	private int count;
 	
-	public DynamicArrays() {
-		array = new int[1];
-		size = 1;
+	public DynamicArrays(int arraySize) {
+		array = new int[arraySize];
+		size = arraySize;
 		count = 0;
 	}
 	
@@ -23,77 +23,81 @@ public class DynamicArrays {
 		}
 		array[count] = element;
 		count++;
+		printArray();
 	}
 	
 	
 	public void addAt(int index, int element) {
-		if(size == count) {
+		if(size == count)
 			growArray();
-		}
-		for(int i= count-1; i>index; i--) {
+
+		for(int i = count-1; i>=index; i--)
 			array[i+1] = array[i];
-		}
+
 		array[index] = element;
 		count++;
+		printArray();
 	}
 	
-	public void remove() {
-		if(count > 0) {
-            count--;
-			array[count] = 0;
-		}
+	public void remove(int element) {
+		int index = linearSearch(element);
+		if (index == -1)
+			System.out.println("Unable to find element");
+		else
+			removeAt(index);
+	}
+
+	public int linearSearch(int element) {
+		int index = -1;
+		for(int i=0;i<count; i++)
+			if (array[i] == element)
+				index = i;
+		return index;
 	}
 	
 	public void removeAt(int index) {
-		for(int i=index; i<count;i++) {
+		for(int i=index; i<count;i++)
 			array[i] = array[i+1];
-		}
+
         count--;
 		array[count] = 0;
+		if(count < size/2)
+			shrinkSize();
+		printArray();
 	}
 	
 	public void shrinkSize() {
-		if(count > 0) {
-			int temp[] = Arrays.copyOf(array, count);
-			array = temp;
-			size = count;
-		}
+		int[] temp = Arrays.copyOf(array, size/2);
+		array = temp;
+		size = size/2;
 	}
 	
 	public void growArray() {
-		int temp[] = new int[2*size];
+		int[] temp = new int[2*size];
 		for(int i=0;i<size; i++ ) {
 			temp[i] = array[i];
 		}
+		//or
+		//int[] temp = Arrays.copyOf(array, 2*size);
 		array = temp;
 		size = 2*size;
 	}
 	
 	public static void main(String[] args) {
-		DynamicArrays arr = new DynamicArrays();
-//		
-//		int[] array = new int[100];
-//		System.out.println(array.length);
-//		int[] newArray = new int[2];
-//		array = newArray;
-//		System.out.println(array.length);
+		DynamicArrays arr = new DynamicArrays(5);
 		
 		arr.add(5);
-		System.out.println(arr.size);
 		arr.addAt(1,6);
-		System.out.println(arr.size);
 		arr.addAt(2, 0);
-		System.out.println(arr.size);
 		arr.add(4);
 		arr.add(6);
 		arr.add(7);
-		System.out.println(arr.size);
 		arr.removeAt(2);
-		arr.remove();
-		arr.shrinkSize();
-		System.out.println(arr.size);
-		arr.shrinkSize();
-		System.out.println(arr.size);
+		arr.remove(4);
+		arr.remove(8);
 	}
 
+	public void printArray() {
+		System.out.println(Arrays.toString(array));
+	}
 }
